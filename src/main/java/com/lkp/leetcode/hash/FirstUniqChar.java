3c9acb66628c7698b1684d9df6dc31a382fd14d5
liukp <85291173@qq.com>
2019-12-25 09:15:27 +0800

    387. 字符串中的第一个唯一字符

---
+++ src/main/java/com/lkp/leetcode/hash/FirstUniqChar.java
@@ -0,0 +1,27 @@
+package com.lkp.leetcode.hash;
+
+import java.util.HashMap;
+
+/**
+ *@className FirstUniqChar
+ *@description 387. 字符串中的第一个唯一字符
+ *@Auther 85291173@qq.com
+ *@Date 2019/12/25 9:08
+ *@Version
+ */
+public class FirstUniqChar {
+	public int firstUniqChar(String s) {
+		HashMap<Character,Integer> map = new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			map.put(c,map.getOrDefault(c,0)+1);
+		}
+		for (int i = 0; i < s.length(); i++) {
+			Integer integer = map.get(s.charAt(i));
+			if(integer <= 1){
+				return  i;
+			}
+		}
+		return  -1  ;
+	}
+}
