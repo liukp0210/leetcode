b83323e965fd0709933ae25633756381401f6670
liukp <85291173@qq.com>
2020-01-16 10:43:18 +0800

    374. 猜数字大小

--- src/main/java/com/lkp/leetcode/BinarySearch/GuessNumber.java
+++ src/main/java/com/lkp/leetcode/BinarySearch/GuessNumber.java
@@ -2,7 +2,7 @@ package com.lkp.leetcode.BinarySearch;
 
 /**
  *@className GuessNumber
- *@description 猜数字大小
+ *@description 374. 猜数字大小
  *@Auther 85291173@qq.com
  *@Date 2019/8/28 16:19
  *@Version
@@ -59,4 +59,22 @@ public class GuessNumber {
 		}
 		return  0;
 	}
+	public int guessNumber2(int n) {
+		int left = 0 ;
+		int right = n;
+		while (left <= right){
+			int mid = (left + right)>>>1;
+			int guess = guess(mid);
+			if(guess == 0 ){
+				return mid;
+			}else if (guess ==  1){
+				left = mid + 1;
+			}else{
+				right = mid - 1;
+			}
+		}
+		return -1;
+	}
+	
+	
 }
